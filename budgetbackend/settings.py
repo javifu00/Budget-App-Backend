93365fd78fb81959893a7c0df0ed41d2d83c94e0
javifu00 <javierfuenmayor00@gmail.com>
2022-06-05 16:28:56 -0400

    Deploying to Heroku

--- budgetbackend/settings.py
+++ budgetbackend/settings.py
@@ -180,9 +180,13 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+django_heroku.settings(locals())
