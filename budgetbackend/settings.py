bb9db72524794161faff586f7880d7791d15a857
javifu00 <javierfuenmayor00@gmail.com>
2022-06-05 18:40:27 -0400

    new

--- budgetbackend/settings.py
+++ budgetbackend/settings.py
@@ -27,7 +27,7 @@ from datetime import timedelta
 SECRET_KEY = "django-insecure-s&=&cp1l3mcbr-wwr)!tz-@^7vcaqq%^k-e9fy%5r-$co^sa-m"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -175,6 +175,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
