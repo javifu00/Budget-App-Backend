6143f7754bb7074a9e37ff7345553dab05ba6f56
javifu00 <javierfuenmayor00@gmail.com>
2022-07-19 00:06:33 -0400

    trying

--- budgetbackend/settings.py
+++ budgetbackend/settings.py
@@ -117,10 +117,10 @@ WSGI_APPLICATION = "budgetbackend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
 
-import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES["default"].update(db_from_env)
